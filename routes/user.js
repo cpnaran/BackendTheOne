be617ed12478ae76a8233e3de0541c7b00ca7f09
otakuict <otaku.ict@gmail.com>
2024-10-13 23:30:07 +0700

    add health check

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
-console.log("testenv", process.env.FRONT_END_BASE_URL);
+
 router.post("/submit", (req, res) => {
   const { userId } = req.query;
   console.log(userId);
   // create user in database
 
   res.json("success");
+});
+router.get("/health", (req, res) => {
+  console.log("testenv", process.env.FRONT_END_BASE_URL);
+  res.json({ messege: "application is ready!" });
 });
 
 module.exports = router;
