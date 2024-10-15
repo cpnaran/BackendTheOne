daa551aa6a2a98e7df5e189f046b46157cbd5446
otakuict <otaku.ict@gmail.com>
2024-10-16 02:16:08 +0700

    add empty route

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import { config } from 'dotenv';
-import express from 'express';
-import mysql from 'mysql2'
+import { config } from "dotenv";
+import express from "express";
+import mysql from "mysql2";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const API_BASE_URL = process.env.API_BASE_URL;
 
-config()
+config();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -15,16 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //import routes
-import webHookRoutes from './src/routes/webhook.js';
-import userRoutes from './src/routes/user.js';
-console.log(new Date())
+import webHookRoutes from "./src/routes/webhook.js";
+import userRoutes from "./src/routes/user.js";
+console.log(new Date());
 app.get("/health", (req, res) => {
-    console.log("testenv", process.env.FRONT_END_BASE_URL);
-    res.json({ messege: "application is ready!" });
+  console.log("testenv", process.env.FRONT_END_BASE_URL);
+  res.json({ messege: "application is ready!" });
 });
-app.use('/webhook', webHookRoutes);
-app.use('/user', userRoutes)
+app.get("/", (req, res) => {
+  res.json({ messege: "success" });
+});
+app.use("/webhook", webHookRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
