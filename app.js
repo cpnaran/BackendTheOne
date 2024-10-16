e4b3eea004fe83921a1bfb09ab4dde3350a80ae7
MUM <otaku.ict@gmail.com>
2024-10-16 13:34:03 +0700

    fix import

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import { config } from 'dotenv';
-import express from 'express';
-import mysql from 'mysql2'
-import scheduleOptionsTask from './src/cron/optionsJob.js'
+import { config } from "dotenv";
+import express from "express";
+import mysql from "mysql2";
+import scheduleOptionsTask from "./src/cron/optionsJob.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,22 +18,22 @@ app.use(express.urlencoded({ extended: true }));
 scheduleOptionsTask();
 
 //import routes
-import webHookRoutes from './src/routes/webhook.js';
-import userRoutes from './src/routes/user.js';
-import optionRoutes from './src/routes/option.js'
+import webHookRoutes from "./src/routes/webhook.js";
+import userRoutes from "./src/routes/user.js";
+import optionRoutes from "./src/routes/option.js";
 console.log(new Date());
 
 app.get("/health", (req, res) => {
-    console.log("testenv", process.env.FRONT_END_BASE_URL);
-    res.json({ messege: "application is ready!" });
+  console.log("testenv", process.env.FRONT_END_BASE_URL);
+  res.json({ messege: "application is ready!" });
 });
 app.get("/", (req, res) => {
-    res.json({ messege: "success" });
+  res.json({ messege: "success" });
 });
-app.use('/webhook', webHookRoutes);
-app.use('/user', userRoutes)
-app.use('/options', optionRoutes)
+app.use("/webhook", webHookRoutes);
+app.use("/user", userRoutes);
+app.use("/options", optionRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
