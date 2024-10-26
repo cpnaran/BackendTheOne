60f14c3d2f495bcd256ca9c60dbbdc60c88dcf28
otakuict <otaku.ict@gmail.com>
2024-10-26 16:56:42 +0700

    add package table

--- src/feature/backOffice.js
+++ src/feature/backOffice.js
@@ -262,3 +262,14 @@ export const deletePackage = async (id) => {
 
   return "success";
 };
+
+export const getPackageTable = async () => {
+  const Packages = await Package.findAll({
+    where: {
+      id: {
+        [Op.ne]: "30d27f15-0ace-4263-b789-1c851d20ac6c",
+      },
+    },
+  });
+  return Packages;
+};
